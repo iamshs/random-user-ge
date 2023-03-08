67fb02b8d230bec1c864abfc429cdda6385d7d5e
Md Sholayman <mdsholayman998@gmail.com>
2023-03-08 21:06:55 +0600

    understanding event module

---
+++ event.js
@@ -0,0 +1,19 @@
+const EventEmitter = require("events")
+const emitter = new EventEmitter()
+
+
+emitter.on("belling" , (period) =>{
+  console.log(`bell ringing because ${period} class has ended `);
+})
+
+// emitter.emit("belling")
+
+// setTimeout(()=>{    
+// emitter.emit("belling")
+// },2000)
+
+setTimeout(() =>{
+
+emitter.emit("belling", "second")
+
+},2000)
--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const fs = require("fs")
 
+
+
+
+
+
+// file-----system
+
+// fs.rename("file" ,"myfile.txt" , (err,data) =>{
+    
+// })
+
 // fs.writeFile("myfile.txt" , "Hello developers,")
 // fs.appendFile("myFile.txt" , "Welcome")
 
-fs.readFile("myFile.txt" , (err,data) =>{
-    console.log(data.toString());
-}) ;
+// fs.readFile("myFile.txt" , (err,data) =>{
+//     console.log(data.toString());
+// }) ;
 // console.log(data)--got buffer;
-console.log("object");
+// console.log("object");
 
